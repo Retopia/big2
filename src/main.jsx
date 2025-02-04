892461ae52f7df1f2c5c6a814050eba7f5424bf9
Preston Tang <prestonleetang0123@gmail.com>
2025-02-04 01:58:43 -0500

    Added speed insights and analytics from vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
-createRoot(document.getElementById('root')).render(<App />);
+createRoot(document.getElementById('root')).render(
+  <>
+    <App />
+    <SpeedInsights />
+    <Analytics />
+  </>
+);
